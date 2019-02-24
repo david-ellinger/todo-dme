3e8dff3d0161546de146273aa430a47621278469
David Ellinger <david.ellinger@me.com>
2019-02-23 22:20:00 -0800

    Setup route infrastructure

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+app.use(helmet());
+const router = express.Router();
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.dbURL || "mongodb://core-db:27017/core", {useNewUrlParser: true})
+
 const port = process.env.PORT || 3000;
 const server = require('http').Server(app);
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(router);
+
+app.use("/api", require('./routes'));
 
 app.get('/', (req,res) => {
     return res.status(200).json({msg: "Hello World"})
