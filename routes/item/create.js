29dbe305093d42ccdb6d8b80de50e7528ccad2bf
David Ellinger <david.ellinger@me.com>
2019-02-23 22:41:55 -0800

    Creation of users and items

--- routes/item/create.js
+++ routes/item/create.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const Item = require('../../models/Item');
+const uuid = require('uuid/v4');
 
-router.get('/', (req,res, next) => {
-    return res.status(200).json({msg: 'user endpoint'});
+router.post('/', (req,res, next) => {
+    if(!req.body) {
+        return res.status(400).json({
+            msg: "Missing required fields"
+        });
+    }
+    const item = new Item({...req.body, _id: uuid()})
+    item.save().then(savedItem => {
+        return res.status(201).json(savedItem);
+    }).catch((error) => {
+        console.log(`Error saving Item: ${error}`);
+        return res.status(500).json({msg: `Error saving item`})
+    });
 })
 
 module.exports = router;
--- routes/user/create.js
+++ routes/user/create.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
-
-router.get('/', (req,res, next) => {
-    return res.status(200).json({msg: 'user endpoint'});
+const User = require('../../models/User')
+const uuid = require('uuid');
+router.post('/', (req,res, next) => {
+    if(!req.body) {
+        return res.status(400).json({
+            msg: "Missing required fields"
+        });
+    }
+    const user = new User({
+        _id: uuid(),
+        username: req.body.username,
+    })
+    user.save().then(savedUser => {
+        return res.status(201).json(savedUser);
+    }).catch((error) => {
+        console.log(`Error saving User: ${error}`);
+        return res.status(500).json({msg: `Error saving user`})
+    });
 })
 
 module.exports = router;
